f7afa2f53f7778fb4719715cd188b0ad53cfe942
efrenruizrubio <efren282@outlook.es>
2022-08-13 18:55:07 -0500

    :sparkles: [ADD]: Added coupons to discount the total price

--- pages/scripts/percentages.js
+++ pages/scripts/percentages.js
@@ -1,21 +1,43 @@
-//const btn = document.querySelector("#btnClick");
+const btn = document.querySelector("#btnClick");
 const result = document.querySelector("#result");
 const inputPrice = document.querySelector("#price");
 const inputDiscount = document.querySelector("#discount");
 
 result.innerText = `Precio con descuento: ${100}`;
 
+const coupon = [
+	{ couponName: "PLTZ2022", discount: 20 },
+	{ couponName: "MASTER2022", discount: 85 },
+];
+
 const calculateDiscount = (price, discount) => {
 	return (price * (100 - discount)) / 100;
 };
 
-document.querySelectorAll(".input").forEach((element) => {
+btn.addEventListener("click", () => {
+	discount = coupon.filter((el) => el.couponName === inputDiscount.value);
+	if (discount.length) {
+		result.innerText = `Precio con el cupón ${
+			discount[0].couponName
+		}: ${calculateDiscount(inputPrice.value, discount[0].discount)}`;
+	}
+});
+
+/* document.querySelectorAll(".input").forEach((element) => {
 	element.addEventListener("input", () => {
+		if (inputDiscount.value > 100) {
+			result.innerText =
+				"El valor del descuento no puede ser menor que 0 y mayor que 100";
+			inputDiscount.classList.add("error");
+			console.log({ inputDiscount });
+			return;
+		}
+
 		result.innerText = `Precio con descuento del ${
 			inputDiscount.value
 		}%: ${calculateDiscount(inputPrice.value, inputDiscount.value)}`;
 	});
-});
+}); */
 
 /* 
 
