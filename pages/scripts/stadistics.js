c9162983e69d4601c1cdffb34b59ba7442848f36
efrenruizrubio <efren282@outlook.es>
2022-08-13 23:16:53 -0500

    :recycle: [REF]: Used class instead of object for stadistics

--- pages/scripts/stadistics.js
+++ pages/scripts/stadistics.js
@@ -1,4 +1,4 @@
-const Stadistics = {};
+/* const Stadistics = {};
 
 Stadistics.calculateAverage = calculateAverage = (arr) => {
 	return (
@@ -25,5 +25,40 @@ Stadistics.calculateModal = calculateModal = (arr) => {
 };
 
 Stadistics.calculateArithmeticAverage = calculateArithmeticAverage = (arr) => {
+	return arr.length / arr.map((el) => 1 / el).reduce((a, b) => a + b);
+}; */
+
+class Stadistics {
+	static calculateAverage;
+	static calculateMedian;
+	static calculateModal;
+	static calculateArithmeticAverage;
+}
+
+Stadistics.calculateAverage = (arr) => {
+	return (
+		arr.reduce((el, acc) => {
+			return el + acc;
+		}) / arr.length
+	);
+};
+
+Stadistics.calculateMedian = (arr) => {
+	const median = arr.length / 2;
+	const ordenedArray = arr.sort((a, b) => a - b);
+	return arr.length % 2 === 0
+		? (ordenedArray[median] + ordenedArray[median - 1]) / 2
+		: ordenedArray[Math.floor(median)];
+};
+
+Stadistics.calculateModal = (arr) => {
+	let obj = {};
+	arr.map((el) => {
+		el in obj ? (obj[el] += 1) : (obj[el] = 1);
+	});
+	console.log(Math.max(...Object.values(obj)));
+};
+
+Stadistics.calculateArithmeticAverage = (arr) => {
 	return arr.length / arr.map((el) => 1 / el).reduce((a, b) => a + b);
 };
